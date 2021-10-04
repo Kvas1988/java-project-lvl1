2b05d13696005cfcb2e7edbe1a9fb8de2250f181
Kvas <shokhin.al@gmail.com>
2021-10-05 02:03:10 +0300

    Task 5. Even Game

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,9 +1,38 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        Scanner sc = new Scanner(System.in);
+        int gameNumber = 0;
 
-        Cli.helloUser();
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Even\n"
+                + "0 - Exit\n"
+                + "Your choice: ");
+
+
+        if (sc.hasNextInt()) { // check if input is Int
+            gameNumber = sc.nextInt(); // get input from user
+        }
+
+        System.out.print("\n");
+
+
+        String username = "";
+        switch (gameNumber) {
+            case 1:
+                username = Cli.getUsername();
+                break;
+            case 2:
+                username = Cli.getUsername();
+                EvenGame.startEvenGame(username);
+                break;
+            default:
+                break;
+        }
     }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,13 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void helloUser() {
+    public static String getUsername() {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("May I have your name? ");
         String name = sc.nextLine();
         System.out.println("Hello, " + name + "!");
+
+        return name;
     }
 }
